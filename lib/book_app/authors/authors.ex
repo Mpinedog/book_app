77e95e0c294ad3bfe3694b7849c9f7c96bc00b81
Borja Manterola <bjmanterola@miuandes.cl>
2025-08-14 20:18:19 -0400

    Authors CRUD

--- lib/book_app/authors/authors.ex
+++ lib/book_app/authors/authors.ex
@@ -18,4 +18,20 @@ defmodule BookApp.Authors do
 	Gets a single author.
 	"""
 	def get_author!(id), do: Repo.get!(Author, id)
+
+	def create_author(attrs \\ %{}) do
+		%Author{}
+		|> Author.changeset(attrs)
+		|> Repo.insert()
+	end
+
+	def update_author(%Author{} = author, attrs) do
+		author
+		|> Author.changeset(attrs)
+		|> Repo.update()
+	end
+
+	def delete_author(%Author{} = author) do
+		Repo.delete(author)
+	end
 end
