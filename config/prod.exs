c3a48af13db434adc8612722236fc12e30d6f7ec
mpinedog <mpinedo1@miuandes.cl>
2025-08-20 11:10:00 -0400

    Regenerate mix.lock for prod build

--- config/prod.exs
+++ config/prod.exs
@@ -16,5 +16,10 @@ config :swoosh, local: false
 # Do not print debug messages in production
 config :logger, level: :info
 
+# Configure your database
+config :book_app, BookApp.Repo,
+  database: System.get_env("DATABASE_PATH") || "priv/repo/book_app_prod.db",
+  pool_size: 5
+
 # Runtime production configuration, including reading
 # of environment variables, is done on config/runtime.exs.
